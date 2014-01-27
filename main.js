e267b7c1ab7675a43a5f28533e27d4601d9cecd0
Jerome Lai <jerome.lai@hyperisland.se>
2014-01-27 13:11:08 +0100

    3rd
    some functionality added

--- main.js
+++ main.js
@@ -1,3 +1,33 @@
-var popcorn = Popcorn( "#video" );
+var $pop = Popcorn( "#video" );
 
-popcorn.autoplay( true );
+// first pause
+$pop.cue( 3, function() {
+	$pop.pause();
+});
+
+//listen for timeupdate
+$pop.on( "timeupdate", function( event ) {
+    // if the video is paused
+    if ( this.paused() ) {
+        // add a footnote!
+        this.footnote({
+        	start: $pop.currentTime(),
+        	end: $pop.currentTime() + 1  > $pop.duration() ? $pop.duration() : $pop.currentTime() + 1,
+        	text: "Footnote at " + $pop.currentTime() + " ",
+        	target: "footnote"
+        });
+    }
+});
+// end of list for timeupdate
+
+$pop.webpage({
+	id: "webpages-a",
+	start: 1,
+	end: 5,
+	src: "http://www.google.com/",
+	target: "webpagediv"
+});
+
+function jumpTo() {
+	$pop.play(15);
+}
