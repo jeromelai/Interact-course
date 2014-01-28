9fffbb3d57a6b7f8014b7e18740479bb646da6f6
Jerome Lai <jerome.lai@hyperisland.se>
2014-01-28 20:28:53 +0100

    beta1

--- main.js
+++ main.js
@@ -1,28 +1,71 @@
 var $pop1 = Popcorn( "#video" );
 
-function jumpTo() {
-	$pop1.play(15);
-}
-
 function changeZIndex(i,id) {
   document.getElementById(id).style.zIndex=i;
 }
 
-$pop1.play();
+// $pop1.play();
+$pop1.autoplay(56);
 
-// pause 1
+// pause d1 - decision1
 $pop1.cue( 56, function() {
 	$pop1.pause();
 	changeZIndex(2, "decision1");
-	changeZIndex(1,'gif1');
-    changeZIndex(0, "canvas");
+	changeZIndex(1, "d1gif");
 });
 
-// pause 2a - have sex
+// pause s1 - have sex
 $pop1.cue( 72, function() {
 	$pop1.pause();
-	changeZIndex(10, "stats1");
-	changeZIndex(3,'gif2');
-    changeZIndex(0, "canvas");
+	changeZIndex(2, "stats1a");
+	changeZIndex(1, "s1gif");
 });
 
+// pause s2 - no sex, masturbate
+$pop1.cue( 89, function() {
+	$pop1.pause();
+	changeZIndex(2, "stats2a");
+	changeZIndex(1, "s2gif");
+});
+
+// pause d2 - Nora decides school or cafe
+$pop1.cue( 131, function() {
+	$pop1.pause();
+	changeZIndex(2, "decision2");
+	changeZIndex(1, "d2gif");
+});
+
+// pause s3 - go to school
+$pop1.cue( 145, function() {
+	$pop1.pause();
+	changeZIndex(2, "stats3");
+	changeZIndex(1, "s3gif");
+});
+
+// pause s4 - go to cafe
+$pop1.cue( 153, function() {
+	$pop1.pause();
+	changeZIndex(2, "stats4");
+	changeZIndex(1, "s4gif");
+});
+
+// pause d3 - banana? cheat?
+$pop1.cue( 189, function() {
+	$pop1.pause();
+	changeZIndex(2, "decision3");
+	changeZIndex(1, "d3gif");
+});
+
+// pause s5 - cheat
+$pop1.cue( 205, function() {
+	$pop1.pause();
+	changeZIndex(2, "stats5");
+	changeZIndex(1, "s5gif");
+});
+
+// pause s6 - don't cheat
+$pop1.cue( 222, function() {
+	$pop1.pause();
+	changeZIndex(2, "stats6");
+	changeZIndex(1, "s6gif");
+});
